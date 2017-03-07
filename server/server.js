f54df160b4926e1377aec3fbe3a8af086e14c3b1
Asad Zubair <asad.zubair@10pearls.local>
2017-03-07 16:59:59 +0500

    fixed heroku port

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 
