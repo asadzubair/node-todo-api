eeaf494d7892d9ce6f2978a0442d3bd19a007458
Asad Zubair <asad.zubair@10pearls.local>
2017-03-08 12:39:34 +0500

    added patch endpoint and modified user model

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -13,11 +14,12 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
+        
     var todo = new Todo({
         text: req.body.text
     });
-    
-    todo.save().then( (doc) => {
+
+    todo.save().then((doc) => {
         res.send(doc);
     }, (e) => {
         res.status(400).send(e);
@@ -73,6 +75,34 @@ app.delete('/todos/:id', (req, res) => {
         return res.status(400).send();
     })
 })
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then( (doc) => {
+        if(!doc) {
+            return res.status(404).send();
+        }
+        res.status(200).send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    }).catch( (e) => {
+        res.status(400).send(e);
+    })
+});
+
 
 app.listen(port, () => {
     console.log(`started on port ${port}`);
