fd32d22532d2e4b79cacd0b5b3faa2f550361ebb
Asad Zubair <asad.zubair@10pearls.local>
2017-03-09 14:47:21 +0500

    hash password before saving

--- play/hashing.js
+++ play/hashing.js
@@ -1,15 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     })
+// });
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var hashedPassword = '$2a$10$bpHy6wtvTU0DKalyGSRi/eRbLHXn6ANAcaZsQlIiobPUG3Y2Dxjsu';
+bcrypt.compare('password', hashedPassword, (err, result) => {
+    console.log(result);
+});
 
-var decoded = jwt.verify(token, '123abcd');
-console.log(decoded);
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abcd');
+// console.log(decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message.toString());
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash')
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -69,6 +70,26 @@ UserSchema.statics.findByToken = function(token) {
         'tokens.access': 'auth'
     })
 }
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    if(user.isModified('password')) {
+        console.log('password is modified');
+        bcrypt.genSalt(10, (err, salt) => {
+            console.log('salt - ', salt);
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                console.log('hash - ', hash);
+                user.password = hash;
+                 next();
+            });
+           
+        })
+        
+    }else {
+        next();
+    }
+})
 
 
 UserSchema.methods.toJSON = function() {
